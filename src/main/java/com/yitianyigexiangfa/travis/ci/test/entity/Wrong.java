19a74d00f30630ca51c00a862432b1fef9183e9a
刘备 <admin@yitianyigexiangfa.com>
2017-09-07 13:49:04 +0800

    create an error

---
+++ src/main/java/com/yitianyigexiangfa/travis/ci/test/entity/Wrong.java
@@ -0,0 +1,10 @@
+package com.yitianyigexiangfa.travis.ci.test.entity;
+
+public class Wrong {
+
+    public static void main(String[] args) {
+        int a, b, c;
+        c = a + b;
+        System.out.println(c);
+    }
+}
